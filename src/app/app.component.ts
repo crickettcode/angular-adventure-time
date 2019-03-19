cb46e6f5ddb5cce029159a7fd297a6abc212e58a
Jonathan Beatty <gongon05@gmail.com>
2019-03-19 14:59:59 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles:[`
+  
+  `] // you can add your styles' here as well
 })
 export class AppComponent {
-  title = 'my-new-app';
+
 }
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: `<app-server-eminem></app-server-eminem>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
