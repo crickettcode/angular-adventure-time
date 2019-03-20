88e419e36d62409163f804b202f1e7571f3e5dfd
Jonathan Beatty <gongon05@gmail.com>
2019-03-19 20:16:35 -0400

    basic css

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
 selector:'add-addition',
-templateUrl:'./add.component.html'
+templateUrl:'./add.component.html',
+styleUrls: ['./add.component.css']
 })
 
 
--- src/app/subtract/subtract.component.ts
+++ src/app/subtract/subtract.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
 selector:'subtract-subtraction',
-templateUrl:'./subtract.component.html'
+templateUrl:'./subtract.component.html',
+styleUrls: ['./subtract.component.css']
 })
 
 
